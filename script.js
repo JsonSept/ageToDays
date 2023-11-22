f12f2a388d97eb4ad34d10fe625bdd436e5ba0a5
Jason <147480420+JsonSept@users.noreply.github.com>
2023-11-22 16:00:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function ageToDays() {
+    let ageInDays = parseFloat(document.getElementById('age').value);
+   
+
+    // Check if the input is a valid number
+    if (isNaN(ageInDays)) {
+        alert("Please enter a valid number for minutes.");
+        return;
+    }
+    // Convert age to days
+    let daysAge = ageInDays * 365.25; 
+
+    // Display the result in the output field
+    document.getElementById('days').value = daysAge.toFixed(2);
+ }
